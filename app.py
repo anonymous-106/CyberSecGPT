1a4de9a5fe563144d4160a442408d93acb10ec65
anonymous-106 <snabeel8210@gmail.com>
2026-06-27 20:59:59 +0530

    trying something

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from utils.pdf_reader import read_pdf
 from utils.chunker import create_chunks
 
-text = read_pdf("uploads/ECCU-Catalog-2025.pdf")
+text = read_pdf("uploads/100 Essential Linux Commands.pdf")
 
 chunks = create_chunks(text, chunk_size=100, overlap_size=20)
+
+print(f"Total Chunks: {len(chunks)}")
+
+for i in range(5):
+    print(f"\n{'=' * 20} Chunk {i+1} {'=' * 20}")
+    print(chunks[i])
